842af301bd87e0b7285f455c460dfd486729f3cd
NuhaSam <99994162+NuhaSam@users.noreply.github.com>
2023-03-29 00:32:48 +0300

    Add files via upload
    Nuha Sammour 220200810

---
+++ src/components/showBooks.vue
@@ -0,0 +1,71 @@
+<template>
+    <div>
+       <h1>Books</h1>
+
+    <table class="table table-striped">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">First</th>
+          <th scope="col">Last</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(book, i) in books" :key="i">
+          <td>{{ book.id }}</td>
+          <td>{{ book.title }}</td>
+          <td>{{ book.author }}</td>
+          <td>{{ book.id }}</td>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+</template>
+<script>
+// import CreateBookRecord from "./CreateBookRecord.vue";
+export default {
+  data: function () {
+    return {
+      showComponent: false,
+      books: [
+        {
+          id: 1,
+          title: "A",
+          author: "Ahmed",
+        },
+        {
+          id: 2,
+          title: "B",
+          author: "Ali",
+        },
+        {
+          id: 3,
+          title: "C",
+          author: "Oday",
+        },
+        {
+          id: 4,
+          title: "D",
+          author: "Mohammed",
+        },
+      ],
+    };
+  },
+//   components: {
+//     CreateBookRecord,
+//   },
+  methods: {
+    createBookRecord() {
+      // this.$modal.show('CreatBookRecord')
+    },
+  },
+  name: "ListBook",
+};
+
+</script>
+<style scoped>
+table {
+    width: 50%;
+    margin: 5% 10% 0 10%;
+}</style>
---
+++ src/components/signIn.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1 id="main-heading">Book Tracker</h1>
+  <div id="signin">
+    <h3 id="signin-heading">Sign in</h3>
+    <form>
+      <div class="signin-input">
+        <label class="signin-label">Email</label>
+        <input type="email" class="signin-inputfield" id="email" v-model="email" required />
+      </div>
+      <div class="signin-input">
+        <label class="signin-label">Password</label>
+        <input type="password" class="signin-inputfield" id="password" v-model="password" required />
+        <p id="forgot-password" @click="forgotPassword()">Forgot Password?</p>
+      </div>
+      <p v-if="errorPresentSignIn" id="error-handling-text">
+        {{ this.errorMessageSignIn }}
+      </p>
+      <p v-if="resetEmailSent && !errorPresentSignIn" id="reset-email-success-text">
+        {{ this.resetEmailMessage }}
+      </p>
+      <button type="button" @click="signIn()" id="signin-btn">Sign In</button>
+      <p id="signup-text">
+        Don't have an account yet?<span id="signup-link" @click="signUpPromptClicked()">Sign up.</span>
+      </p>
+    </form>
+
+  </div>
+</template>
+  
+<script>
+import axios from 'axios';
+export default {
+  name: "SignIn",
+  data() {
+    return {
+      email: '',
+      password: '',
+    }
+  }
+  , methods: {
+    async signIn() {
+      let result = await axios.post('http://localhost:8080/bookClasses ', {
+        email:this.email,
+    password: this.password
+
+  });
+  console.log(result);
+  if(result.status == 200){
+    alert('Done !!');
+  }
+      console.log('signing in **')
+}
+  }
+};
+</script>
+  
+<style scoped>
+@import '../assets/css/signIn.css';
+</style>
+  
